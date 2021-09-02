db3fb678aa915ce020bcc63fb9502d6df9a5e458
Alec-Sandoval <63359143+Alec-Sandoval@users.noreply.github.com>
2021-09-02 00:58:36 -0500

    Initialize 3-models-in-angular

---
+++ src/app/messaging/messaging.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mcc-messaging',
+  templateUrl: './messaging.component.html',
+  styleUrls: ['./messaging.component.scss']
+})
+export class MessagingComponent implements OnInit {
+  public messages = [
+    {
+      sender: 'John Smith',
+      recipient: 'Jane Doe',
+      dateSent: new Date('2021-04-01T14:48:00.000Z'),
+      subject: 'Lorem ipsum dolor sit amet',
+      message: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }, {
+      sender: 'James Allan',
+      recipient: 'Jane Doe',
+      dateSent: new Date('2021-04-01T15:34:00.000Z'),
+      subject: 'Consectetur adipiscing elit',
+      message: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
